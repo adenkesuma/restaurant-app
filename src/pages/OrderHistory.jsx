e21add17d180b84e69acb0beea9d803b8a938718
raymondtju <raymond.rtju@gmail.com>
2023-07-14 22:17:53 +0700

    fix: order his err

--- src/pages/OrderHistory.jsx
+++ src/pages/OrderHistory.jsx
@@ -5,11 +5,10 @@ import { useOrder } from "../states/useOrder";
 const OrderHistory = () => {
   const order = useOrder();
 
-  const [data, setData] = useState([]);
-
+  const [data, setData] = useState();
   useEffect(() => {
     setData(order.history);
-  }, [order]);
+  }, [order.history]);
 
   return (
     <div className="bg-[#1a1a1a] min-h-[60vh]">
@@ -19,41 +18,47 @@ const OrderHistory = () => {
       </div>
 
       <div className="w-full px-8">
-        {data.length !== 0 ? (
+        {data?.length !== 0 ? (
+          data &&
           data.map((item, i) => (
             <div
               className="w-full my-[1rem] mx-0 flex flex-col border p-1 rounded-md"
               key={i}
             >
-              {item.data.map((itm) => (
-                <div key={itm.id}>
-                  <div className="flex justify-between items-center">
-                    <div className="">
-                      <p className="p__cormorant" style={{ color: "#DCCA87" }}>
-                        {itm.title}
-                      </p>
+              {item.data
+                ? item?.data?.map((itm) => (
+                    <div key={itm.id}>
+                      <div className="flex justify-between items-center">
+                        <div className="">
+                          <p
+                            className="p__cormorant"
+                            style={{ color: "#DCCA87" }}
+                          >
+                            {itm.title}
+                          </p>
+                        </div>
+                        {/* <div className="mr-4 w-[90px] h-[1px] bg-[#DCCA87]" /> */}
+                        <p className="p__cormorant pl-2">~{itm.price}</p>
+                        <div className="flex flex-1 flex-row gap-1 justify-end items-center">
+                          <p className="p__cormorant text-center mx-2">
+                            {itm.count} plates
+                          </p>
+                        </div>
+                      </div>
                     </div>
-                    {/* <div className="mr-4 w-[90px] h-[1px] bg-[#DCCA87]" /> */}
-                    <p className="p__cormorant pl-2">~{itm.price}</p>
-                    <div className="flex flex-1 flex-row gap-1 justify-end items-center">
-                      <p className="p__cormorant text-center mx-2">
-                        {itm.count} plates
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              ))}
+                  ))
+                : window.location.reload()}
 
               <div className="border-t py-4">
                 <div className="flex justify-between">
                   <p className="p__cormorant">Total</p>
                   <p className="p__cormorant">
-                    {item.data.length} dishes, {item.plates} plates
+                    {item?.data?.length} dishes, {item?.plates} plates
                   </p>
                 </div>
                 <div className="flex justify-between">
                   <p className="p__cormorant">Total Payment</p>
-                  <p className="p__cormorant">${item.totalPayment}</p>
+                  <p className="p__cormorant">${item?.totalPayment}</p>
                 </div>
               </div>
             </div>
